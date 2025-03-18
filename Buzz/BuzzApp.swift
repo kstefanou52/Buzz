1693c90e8cfad5333a67062b05a4c278cd5ab550
Kostis Stefanou <kostistefanou@gmail.com>
2025-03-18 11:04:21 +0200

    Initialise Project.

---
+++ Buzz/BuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzApp.swift
+//  Buzz
+//
+//  Created by Kostis Stefanou on 18/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buzz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Buzz
+//
+//  Created by Kostis Stefanou on 18/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
