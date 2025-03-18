5143bc09facfbe958dc29cfa0ab0cb10fd5027da
Kostis Stefanou <kostistefanou@gmail.com>
2025-03-18 11:12:15 +0200

    Organize Project.

--- Buzz/BuzzApp.swift
+++ Buzz/BuzzApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BuzzApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginUI()
         }
     }
 }
---
+++ Buzz/UI/Login/LoginUI.swift
@@ -0,0 +1,18 @@
+//
+//  LoginUI.swift
+//  Buzz
+//
+//  Created by Kostis Stefanou on 18/3/25.
+//
+
+import SwiftUI
+
+struct LoginUI: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoginUI()
+}
